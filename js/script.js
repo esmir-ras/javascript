fdfc9dd96a5861f4d2cafd4abbcaceb39674ec70
Esmir Rastoder <73173141+esmir-ras@users.noreply.github.com>
2021-03-07 10:56:56 +0100

    Javascript

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+// Question1
+
+var myName = "esmir";
+console.log(myName);
+
+var result = 10 + 5;
+console.log(result);
+
+var isLoggedIn = true;
+console.log(isLoggedIn);
+
+// question 2
+
+var firstName = "Esmir";
+var lastName = "Rastoder";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// question 3
+
+var pet = "frog";
+console.log(pet);
+
+console.log("The type of frog is pet", typeof pet);
+
+// question 4
+
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+    console.log("The order shipped")
+} else {
+    console.log("The order did not ship")
+}
+
+
+// question 5
+
+for (var i = 7; i <= 13; i++) {
+	console.log(i);
+}
